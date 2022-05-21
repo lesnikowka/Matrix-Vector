08687b0985f32be3c8b2e6ad82f63be441275f13
lesnikowka <92714965+lesnikowka@users.noreply.github.com>
2022-05-21 14:31:12 +0300

    Add files via upload

--- source.cpp
+++ source.cpp
@@ -4,6 +4,20 @@
 
 
 int main() {
+	int m, n;
+	std::cout << "Enter size: ";
+	std::cin >> m>> n;
+	Matrix a(m, n), b(m, n), c(n,m), d;
+
+	std::cin >> a>> b>> c;
+
+	d = a + b;
+	std::cout << d;
+	d = a * c;
+	std::cout << d;
+	a += b;
+	std::cout << a;
+
 
 	return 0;
 }
